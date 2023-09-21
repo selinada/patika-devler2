d06e61a446478341d443b4ea23844285f8fdb5fe
selinada <114803176+selinada@users.noreply.github.com>
2023-09-21 18:57:05 +0300

    Add files via upload

---
+++ elmas.java
@@ -0,0 +1,45 @@
+package patikadev;
+
+import java.util.Scanner;
+
+public class elmas {
+	  
+	
+	public static void main(String[] args) {
+	     int satir;
+		 Scanner input = new Scanner(System.in);
+	        
+	     System.out.print("yüksekliği girin: ");
+		 satir= input.nextInt();
+		 
+//		 satırları yaz
+		 for(int i=1;i<=satir;i++) {
+			 
+//	     boslukları ekle
+			 for(int j = 1;j<=satir-i;j++) {
+				 System.out.print(" ");
+			 }
+//		 yıldızları ekle
+			 for(int k = 1;k<=2*i-1;k++) {
+				 System.out.print("*");
+			 }
+			 System.out.println();
+			 
+		 }
+		 for(int i=satir-1;i>0;i--) {
+			 
+			 for(int j=1;j<=satir-i;j++) {
+				 System.out.print(" ");
+			 }
+			 for(int k=1;k<=2*i-1;k++) {
+				 System.out.print("*");
+			 }
+			 System.out.println();
+			 
+		 }
+		 
+		 
+
+	}
+
+}
